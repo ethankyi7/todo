acbd84ff137fe7fe88875d574c15408d6910b710
Ethan Kyi <Ethankyi79@gmail.com>
2023-07-08 23:39:50 -0400

    modal and useState implemented

--- app/modal.js
+++ app/modal.js
@@ -1,9 +1,15 @@
+import TextField from '@mui/material/TextField';
 import React from 'react';
 
 export default function modal({open, onClose, children}) {
     return(
-        <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center transition-colors`}>
-
+        <div onClick = {onClose} className = {`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visibile bg-black/20" : "invisible"}`}>
+            <div className = {`bg-white rounded-1g shadow p-6 transition-all ${open ? "scale-1000 opacity-100" : "scale-125 opacity-0"}`} style={{ width: '700px', height: '600px' }}>
+                {children}
+                <h1 className = {`flex justify-center items-center text-3xl`}>add a new task</h1>
+                <TextField fullWidth id="outlined-basic" label="Task Name" variant="outlined" margin = "normal"/>
+                <TextField fullWidth id="outlined-basic" label="description" variant="outlined" multiline rows = {4}/> 
+            </div>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 "use client";
 import { useState } from "react"
+import Modal from "/app/Modal.js"
 
 export default function Home() {
-  //const [showPopup, setShowPopup] = React.useState(false);
+  const [open, setOpen] = useState(false);
+
   return (
     <main>
-      <header className="flex justify-between items-center mb-4" style={{ fontSize: '36px' }}>
+      <header className="flex justify-between items-center mb-4 text-3xl">
         <h1 className = "text-slate-300">todo</h1>
-        <button className="text-slate-300">new task</button>
+        <button className="text-slate-300" onClick={() => setOpen(true)}>new task</button>
       </header>
+      <Modal open = {open} onClose = {() => setOpen(false)}/>
     </main>
   )
 }
