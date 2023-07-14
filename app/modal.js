4f20e158a60333352f968de18c20b810401d6f74
Ethan Kyi <Ethankyi79@gmail.com>
2023-07-13 22:20:51 -0400

    modal ui finished

--- app/modal.js
+++ app/modal.js
@@ -1,14 +1,66 @@
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Slider from '@mui/material/Slider';
 import React from 'react';
+
+const marks = [
+    {
+        value: 1,
+        label: "irrelevanat",
+    },
+    {
+        value: 2,
+        label:  " ",
+    },
+    {
+        value: 3,
+        label:  " ",
+    },
+    {
+        value: 4,
+        label:  " ",
+    },
+    {
+        value: 5,
+        label:  "relevant",
+    },
+    {
+        value: 6,
+        label:  " ",
+    },
+    {
+        value: 7,
+        label:  " ",
+    },
+    {
+        value: 8,
+        label:  " ",
+    },
+    {
+        value: 9,
+        label:  " ",
+    },
+    {
+        value: 10,
+        label:  "urgent",
+    },
+]
 
 export default function modal({open, onClose, children}) {
     return(
-        <div onClick = {onClose} className = {`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visibile bg-black/20" : "invisible"}`}>
-            <div className = {`bg-white rounded-1g shadow p-6 transition-all ${open ? "scale-1000 opacity-100" : "scale-125 opacity-0"}`} style={{ width: '700px', height: '600px' }}>
+        <div className = {`fixed inset-0 flex justify-center items-center transition-colors ${open ? "visibile bg-black/20" : "invisible"}`}>
+            <div className = {`bg-white rounded-1g shadow p-6 transition-all ${open ? "scale-1000 opacity-100" : "scale-125 opacity-0"}`} style={{ width: '600px', height: '500px' }}>
                 {children}
                 <h1 className = {`flex justify-center items-center text-3xl`}>add a new task</h1>
                 <TextField fullWidth id="outlined-basic" label="Task Name" variant="outlined" margin = "normal"/>
-                <TextField fullWidth id="outlined-basic" label="description" variant="outlined" multiline rows = {4}/> 
+                <TextField fullWidth id="outlined-basic" label="description" variant="outlined" multiline rows = {4} style= {{marginTop: "10px"}}/> 
+                <div style={{ width: '450px', margin: '0 auto', marginTop: "50px"}}>
+                    <Slider step={1} marks={marks} min={1} max={10} valueLabelDisplay="auto" />
+                </div>
+                <div className="flex justify-between mt-auto mb-3 scale-250" style={{marginTop: "50px"}}>
+                    <Button variant="outlined" onClick={onClose} size="large">Cancel</Button>
+                    <Button variant="outlined" size="large">Confirm</Button>
+                </div>
             </div>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
 
   return (
     <main>
-      <header className="flex justify-between items-center mb-4 text-3xl">
-        <h1 className = "text-slate-300">todo</h1>
-        <button className="text-slate-300" onClick={() => setOpen(true)}>new task</button>
+      <header className="flex justify-between items-center mb-4 text-3xl w-full">
+        <h1 className = "text-slate-300 m1-auto">todo</h1>
+        <button className="text-slate-300 m1-auto" onClick={() => setOpen(true)}>new task</button>
       </header>
       <Modal open = {open} onClose = {() => setOpen(false)}/>
     </main>
